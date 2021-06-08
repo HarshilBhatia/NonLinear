b0313a7f5220500d99f81a5ae35ec5232aeb07b8
Harshil Bhatia <harshilbhatia2001@gmail.com>
2021-06-08 19:55:12 +0200

    3D plot upd

--- inverse3d.py
+++ inverse3d.py
@@ -6,7 +6,6 @@ import matplotlib as mpl
 from helper.Inverse import inverse
 
 import warnings
-warnings.filterwarnings("ignore", category=FutureWarning)
 
 
 def colorFader(c1, c2, mix=0):
@@ -16,28 +15,33 @@ def colorFader(c1, c2, mix=0):
     return mpl.colors.to_hex((1 - mix) * c1 + mix * c2)
 
 
-
-P = 0.9
-eta = 0.88
-xx, yy = np.meshgrid(np.linspace(0.01, 2, 5), np.linspace(0.01, 2, 5))
+warnings.filterwarnings("ignore", category=FutureWarning)
 
 fig = plt.figure()
 ax = plt.axes(projection="3d")
 
-c1 = "#1f77b4"  
-c2 = "red"
-n = 15
+c1 = "#1f77b4"  # blue
+c2 = "black"
 
 
-# Plots the stepwise evolution of points on the bloch sphere
+n = 8 #max number of iterations 
+eta = 0.88
+P = 0.56 
+xx, yy = np.meshgrid(np.linspace(0.01, 2, 3), np.linspace(0.01, 2, 3)) 
+# meshgrid with evenly distributed points
+
+
 def plot(Uo, Vo, Wo):
+    # l1 = []
     ul, vl, wl = [], [], []
-    for itr in range(15):
+
+    for itr in range(n):
         if np.iscomplex(Wo) or np.iscomplex(Uo) or np.iscomplex(Vo):
             return
 
         if Uo ** 2 + Vo ** 2 + Wo ** 2 > 1:
             ax.scatter3D(Uo, Vo, Wo, color="red")
+            # print(Uo, Vo, Wo, (Uo ** 2 + Vo ** 2 + Wo ** 2 + 1) / 2)
             ul.append(Uo)
             vl.append(Vo)
             wl.append(Wo)
@@ -54,6 +58,8 @@ def plot(Uo, Vo, Wo):
         Uo, Vo, Wo = u, v, w
 
     ax.plot3D(ul, vl, wl, color="gray")
+
+
 
 
 for row in range(len(xx)):
@@ -79,7 +85,6 @@ for row in range(len(xx)):
         if u != 0:
             plot(u, v, w)
 
-
 #Plot sphere
 u, v = np.mgrid[0 : 2 * np.pi : 20j, 0 : np.pi : 10j]
 x = np.cos(u) * np.sin(v)
@@ -87,5 +92,5 @@ y = np.sin(u) * np.sin(v)
 z = np.cos(v)
 ax.plot_wireframe(x, y, z, color="#d3d3d3")
 
-
 plt.show()
+
